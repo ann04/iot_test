922478e5e9cdc4db6de9dd29a5eace3776b3a8ef
ann04 <ka19932000@hanmail.net>
2016-09-19 14:37:34 +0900

    commit

--- SumProject/src/sum/SumPro.java
+++ SumProject/src/sum/SumPro.java
@@ -1,9 +1,16 @@
 package sum;
 
+import java.util.Scanner;
+
 public class SumPro {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("숫자1입력>>");
+		int num1 = sc.nextInt();
+		System.out.print("숫자2입력>>");
+		int num2 = sc.nextInt();
+		System.out.println("합:"+(num1+num2));
 	}
 
 }
